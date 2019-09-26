13afdd8c6498244ebd4b1cb8b36a9a6ce6ac8d32
pooja <gupta.pooja03@gmail.com>
2019-09-26 12:28:38 +0530

    code commited on 26th SEP

---
+++ FileDemo2.java
@@ -0,0 +1,14 @@
+import java.io.*;  
+public class FileDemo2 {  
+    public static void main(String[] args) {  
+  
+        String path = "";  
+        boolean bool = false;  
+        try {  
+            System.out.println("Added code 1");
+        } catch (Exception e) {  
+            // if any error occurs  
+            e.printStackTrace();  
+        }  
+    }  
+}  
